a4f0b6a060fbf2ae01abe2e416bd8d54febe292d
Kent Liu <kentliu@gmail.com>
2018-11-14 17:13:47 -0800

    crud

--- server/index.js
+++ server/index.js
@@ -4,6 +4,7 @@ const path = require('path');
 
 const { PriceDataDay } = require('../database/PriceDataDay.js');
 const db = require('../database/index.js');
+const con = require('../database/controllers.js');
 
 const app = express();
 const PORT = 3001;
@@ -12,17 +13,66 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, '/../client/dist')));
 
+app.route('/api/symbol/:id/day')
+  .get((req, res) => {
+    const { id } = req.params;
 
-app.get('/api/symbol/:id/day', (req, res) => {
-  const { id } = req.params;
-  PriceDataDay.find({ id }, null, { sort: { date: 1 } }, (error, results) => {
-    if (error) {
-      res.status(500).send(error);
-    } else {
-      res.status(200).send(results);
-    }
+    const resSender = (error, results) => {
+      if (error) {
+        res.status(500).send(error);
+      } else {
+        res.status(200).send(results);
+      }
+    };
+
+    con.read(id, resSender);
+  })
+  .post((req, res) => {
+    const { id } = req.params;
+
+    const item = Object.assign({}, req.body);
+    item.id = id;
+
+    const resSender = (error, results) => {
+      if (error) {
+        res.status(500).send(error);
+      } else {
+        res.status(200).send(results);
+      }
+    };
+
+    con.create(item, resSender);
+  })
+  .put((req, res) => {
+    const { id } = req.params;
+
+    const item = Object.assign({}, req.body);
+    item.id = id;
+
+    const resSender = (error, results) => {
+      if (error) {
+        res.status(500).send(error);
+      } else {
+        res.status(200).send(results);
+      }
+    };
+
+    con.update(id, item, resSender);
+  })
+  .delete((req, res) => {
+    const { id } = req.params;
+
+    const resSender = (error, results) => {
+      if (error) {
+        res.status(500).send(error);
+      } else {
+        res.status(200).send(results);
+      }
+    };
+
+    con.remove(id, resSender);
   });
-});
+
 
 app.get('/api/symbol/:id/week', (req, res) => {
   const { id } = req.params;
